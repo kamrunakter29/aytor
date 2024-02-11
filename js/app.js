4a4b84c4197c874ac6df80520d949ad979d6d9fc
Tanje <146099481+kamrunakter29@users.noreply.github.com>
2024-02-12 00:52:18 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(function (){
+    /banner slider/ 
+$('.bannerSlider').slick({
+    arrows:false,
+    dots:true,
+    dotsClass: "bannerDots container",
+    autoplay:true,
+});	
+})
+$(".bannerSliderSm").slick({
+    arrows: false,
+    autoplaySpeed: 2000,
+  });
+  
+ 
